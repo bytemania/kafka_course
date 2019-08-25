3eee806ab409e6b4356f8314fbe8cb13d605db7a
sergio <pg11024@gmail.com>
2019-08-25 21:35:00 +0100

    Consummers

---
+++ kafka-consumer-elasticsearch/src/main/java/com/github/bytemania/kafka/tutorial3/ElasticSearchConsumer.java
@@ -0,0 +1,114 @@
+package com.github.bytemania.kafka.tutorial3;
+
+import com.google.gson.JsonParser;
+import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.elasticsearch.action.bulk.BulkRequest;
+import org.elasticsearch.action.bulk.BulkResponse;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestClientBuilder;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.xcontent.XContentType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.time.Duration;
+import java.util.List;
+import java.util.Properties;
+
+public class ElasticSearchConsumer {
+
+    private static final JsonParser JSON_PARSER = new JsonParser();
+
+    private static RestHighLevelClient createClient() {
+        String hostname = "kafka-course-7594258813.eu-west-1.bonsaisearch.net";
+        String username = "da85lnjk10";
+        String password = "2bd5dnp3vb";
+
+        final CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+        credentialsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(username, password));
+
+        RestClientBuilder builder = RestClient.builder(new HttpHost(hostname, 443, "https"))
+                .setHttpClientConfigCallback(httpAsyncClientBuilder -> httpAsyncClientBuilder.setDefaultCredentialsProvider(credentialsProvider));
+
+        return new RestHighLevelClient(builder);
+    }
+
+    private static KafkaConsumer<String, String> createConsumer(String topic) {
+        String bootstrapServers = "127.0.0.1:9092";
+        String groupId = "kafka-demo-elastic-search";
+
+
+        Properties properties = new Properties();
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        properties.setProperty(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false");
+        properties.setProperty(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, "100");
+
+
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(properties);
+        consumer.subscribe(List.of(topic));
+        return consumer;
+    }
+
+    public static void main(String[] args) throws IOException {
+        Logger logger = LoggerFactory.getLogger(ElasticSearchConsumer.class.getName());
+
+        RestHighLevelClient client = createClient();
+
+        KafkaConsumer<String, String> consumer = createConsumer("twitter_tweets");
+
+        while (true) {
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+
+            Integer recordCount = records.count();
+            logger.info("Received " + recordCount + " records");
+            BulkRequest bulkRequest = new BulkRequest();
+            for (var record : records) {
+                try {
+                    String id = extractIdFromTweet(record.value());
+                    String jsonString = record.value();
+                    IndexRequest indexRequest = new IndexRequest("twitter", "tweets", id).source(jsonString, XContentType.JSON);
+
+                    bulkRequest.add(indexRequest);
+                } catch (NullPointerException e) {
+                    logger.warn("skipping bad data: " + record.value());
+                    recordCount--;
+                }
+            }
+
+            if (recordCount > 0) {
+                BulkResponse bulkItemResponses = client.bulk(bulkRequest, RequestOptions.DEFAULT);
+                logger.info("Commiting offsets...");
+                consumer.commitSync();
+                logger.info("Offsets have been commited");
+
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        //client.close();
+    }
+
+    private static String extractIdFromTweet(String tweetJson) {
+        return JSON_PARSER.parse(tweetJson).getAsJsonObject().get("id_str").getAsString();
+    }
+
+}
